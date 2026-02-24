965288acfd5a66d7ab5bc31712f366abbe8e1d0a
Enzo <enzo.daloiadignazio@gmail.com>
2026-02-24 17:30:26 +0100

    comming soon

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,50 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    // #[Route('/index', name: 'app_index')]
+    // public function index(): Response
+    // {
+    //     return $this->render('index/index.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
+
+    // #[Route('/prestation', name: 'app_prestation')]
+    // public function prestation(): Response
+    // {
+    //     return $this->render('index/prestation.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
+
+    // #[Route('/qui', name: 'app_who')]
+    // public function who(): Response
+    // {
+    //     return $this->render('index/who.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
+
+    // #[Route('/equipe', name: 'app_team')]
+    // public function team(): Response
+    // {
+    //     return $this->render('index/team.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
+
+    // #[Route('/process', name: 'app_process')]
+    // public function process(): Response
+    // {
+    //     return $this->render('index/process.html.twig', [
+    //         'controller_name' => 'IndexController',
+    //     ]);
+    // }
+
+    #[Route('/', name: 'app_coming_soon')]
+    public function comingSoon(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+        return $this->render('index/coming.html.twig', [
         ]);
     }
 }
